84e1f6e57d9603de802d84e5cb8c6713ad5e3258
shr2911 <68686711+shr2911@users.noreply.github.com>
2020-07-30 16:00:11 +0530

    Update terraformAction.groovy

--- vars/terraformAction.groovy
+++ vars/terraformAction.groovy
@@ -1,8 +1,3 @@
 def terraformAction(String tfAction){
-    if (tfAction == "init") {
-     echo "Hello World"
      sh "terraform ${tfAction} $terraform_path/"
-    } else {
-     sh "terraform ${tfAction} $terraform_path/"
-    }
 }
